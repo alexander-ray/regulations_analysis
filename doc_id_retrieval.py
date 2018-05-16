a145913b959ae765b1eeb35e5e94f9483645648c
alexander-ray <1alexray@gmail.com>
2018-05-15 19:12:40 -0500

    Simplified

--- doc_id_retrieval.py
+++ doc_id_retrieval.py
@@ -3,9 +3,8 @@ import http.client
 conn = http.client.HTTPSConnection("www.regulations.gov")
 
 headers = {
-    'cache-control': "no-cache",
-    'postman-token': "81936c95-6fc7-ea22-a9be-7abe7c45cab8"
-    }
+    'cache-control': "no-cache"
+}
 
 conn.request("GET", "/exportdocket?docketId=BIS-2018-0006", headers=headers)
 
